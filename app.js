cee1eab841c928891340bbbf6c0932c56d20a85e
benn511 <bgomez190@gmail.com>
2020-08-09 03:15:03 -0500

    Main event handler uses new methods to compare cards.

--- app.js
+++ app.js
@@ -1,60 +1,81 @@
-const initSetup = () => {
-  //Push cards into array
-  for (let i = 1; i <= numPairs; i++) {
-    cards.push(i);
-    cards.push(i);
-  }
-  //Add event handlers to all cards
-  for (cardDiv of cardDivs) {
-    cardDiv.addEventListener("click", handleCardClick);
-  }
-  //Reset button event handler
-  resetBtn.addEventListener("click", resetGame);
+let game = new Memory();
+//Setup init vars of the game
+//&&Need to allow a glimpse to the player at beginning of game to give a chance at winning
+game.init();
 
-  //Setup fresh board
-  resetGame();
-};
-//Event handlers
-const handleCardClick = (e) => {
-  if (points < maxPoints) {
-    //Game is live
+//Grab all game cards
+htmlGridItems = document.querySelectorAll(".grid-item");
 
-    //Don't allow user to pick empty card
-    if (e.target.id == "" && cardsPicked.length !== 2) {
-      console.log("Can't pick null card!");
-      return;
-    }
+//Find the font awesome class from the event
+// e.currentTarget.firstElementChild.classList[1]
 
-    if (cardsPicked.length === 0) {
-      handleFirstCard(e);
-    } else if (cardsPicked.length === 1) {
-      handleSecondCard(e);
-    } else if (cardsPicked.length === 2) {
-      if (isMatch()) {
-        //Update score
-        points++;
-        score.innerHTML = `Score: ${points}`;
-        //Remove cards from board
-        rmPair();
-      } else {
-        //Flip cards back over
-        flipPair();
-      }
+//Main Game event handler
+for (const gridItem of htmlGridItems) {
+  gridItem.addEventListener("click", (e) => {
+    game.handleCard(e.currentTarget);
+    game.checkMatch();
+    // handleCard();
+    // checkForWin(); //max points?
+  });
+}
 
-      //Clear stack regardless of right or wrong pair
-      cardsPicked.pop();
-      cardsPicked.pop();
-    } else {
-      console.log(
-        "Game glitched! Cards picked over or underflowed",
-        "Cards picked length: ",
-        cardsPicked
-      );
-    }
-  } else {
-    //No more pairs are available
-    console.log("Congratulations on finding all pairs! You win!");
-    message.classList.add("win");
-  }
-};
-initSetup();
+// const initSetup = () => {
+//   //Push cards into array
+//   for (let i = 1; i <= numPairs; i++) {
+//     cards.push(i);
+//     cards.push(i);
+//   }
+//   //Add event handlers to all cards
+//   for (cardDiv of cardDivs) {
+//     cardDiv.addEventListener("click", handleCardClick);
+//   }
+//   //Reset button event handler
+//   resetBtn.addEventListener("click", resetGame);
+
+//   //Setup fresh board
+//   resetGame();
+// };
+// //Event handlers
+// const handleCardClick = (e) => {
+//   if (points < maxPoints) {
+//     //Game is live
+
+//     //Don't allow user to pick empty card
+//     if (e.target.id == "" && cardsPicked.length !== 2) {
+//       console.log("Can't pick null card!");
+//       return;
+//     }
+
+//     if (cardsPicked.length === 0) {
+//       handleFirstCard(e);
+//     } else if (cardsPicked.length === 1) {
+//       handleSecondCard(e);
+//     } else if (cardsPicked.length === 2) {
+//       if (isMatch()) {
+//         //Update score
+//         points++;
+//         score.innerHTML = `Score: ${points}`;
+//         //Remove cards from board
+//         rmPair();
+//       } else {
+//         //Flip cards back over
+//         flipPair();
+//       }
+
+//       //Clear stack regardless of right or wrong pair
+//       cardsPicked.pop();
+//       cardsPicked.pop();
+//     } else {
+//       console.log(
+//         "Game glitched! Cards picked over or underflowed",
+//         "Cards picked length: ",
+//         cardsPicked
+//       );
+//     }
+//   } else {
+//     //No more pairs are available
+//     console.log("Congratulations on finding all pairs! You win!");
+//     message.classList.add("win");
+//   }
+// };
+// initSetup();
--- game.js
+++ game.js
@@ -4,8 +4,8 @@ class Memory {
     //Unique stats and info
     this.htmlPairs = document.querySelector("#pairs");
     this.htmlFlips = document.querySelector("#flips");
-    //Game Cards
     this.htmlIcons = document.querySelectorAll(".fas");
+    this.htmlGridItems = document.querySelectorAll(".grid-item");
     this.icons = [
       "fa-bomb",
       "fa-anchor",
@@ -24,13 +24,7 @@ class Memory {
     this.numPairs = 0;
     this.maxPoints = 10;
     this.cardsPicked = [];
-    this.page = new Page();
-  }
-  shuffleCards() {
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
-    }
+    this.clicks = 0;
   }
   init() {
     //Add two cards for each icon
@@ -38,6 +32,63 @@ class Memory {
       this.cards.push(icon);
       this.cards.push(icon);
     });
+
     this.shuffleCards();
+
+    //Add the corresponding icon class to each html game card
+    for (let i = 0; i < this.cards.length; i++) {
+      this.htmlIcons[i].classList.add(this.cards[i]);
+    }
+  }
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  //Event methods
+  handleCard(card) {
+    if (card) {
+      if (card.classList[1] == "clicked") {
+        console.log("Please choose a different card");
+        return;
+      } else {
+        card.classList.add("clicked");
+        this.cardsPicked.push(card);
+      }
+    } else {
+      console.error("Problem with card passed.");
+    }
+  }
+
+  checkMatch() {
+    if (this.cardsPicked.length == 2) {
+      //Check for match
+      let icon1 = cardsPicked[0].firstElementChild[1];
+      let icon2 = cardsPicked[1].firstElementChild[1];
+      if (icon1 == icon2) {
+        //Increase points
+        this.numPairs++;
+        //Make cards unplayable now
+      } else {
+        //unflip cards
+      }
+    } else {
+      return;
+    }
+  }
+
+  checkForWin() {
+    //Is game over?
+    //Check for max points
+  }
+
+  //Html updaters
+  setHtmlFlips() {
+    this.flips.innerHTML = `Flips: ${flips}`;
+  }
+  setHtmlPairs() {
+    this.score.innerHTML = `Score: ${score}`;
   }
 }
